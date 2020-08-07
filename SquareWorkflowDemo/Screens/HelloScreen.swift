8dfb166ffc621a859eeb5267cb68f063e15f01fd
AlexandruIstrate <38143225+AlexandruIstrate@users.noreply.github.com>
2020-08-07 16:43:46 +0300

    Added workflows

---
+++ SquareWorkflowDemo/Screens/HelloScreen.swift
@@ -0,0 +1,9 @@
+//
+//  HelloScreen.swift
+//  SquareWorkflowDemo
+//
+//  Created by Alexandru Istrate on 07/08/2020.
+//  Copyright © 2020 Alexandru Istrate. All rights reserved.
+//
+
+import Foundation
