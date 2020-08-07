07f9d008b4a2d02aef210a0aa8eca5ba7ababb71
AlexandruIstrate <38143225+AlexandruIstrate@users.noreply.github.com>
2020-08-07 15:46:32 +0300

    Initial commit

---
+++ SquareWorkflowDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SquareWorkflowDemo
+//
+//  Created by Alexandru Istrate on 07/08/2020.
+//  Copyright © 2020 Alexandru Istrate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
